1783cb468bdb62028f84c998e328a4a02070ea3a
Jan Mandel <jan.mandel@gmail.com>
2017-03-19 00:39:03 -0400

    added scripts to join local catalogs to global

--- src/join_catalog.py
+++ src/join_catalog.py
@@ -1,15 +1,27 @@
 import json,glob,logging
 import os.path as osp
-
+import collections
+import lock
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 root=osp.abspath('fdds/simulations')
-files = glob.glob(osp.join(root,'catalog.json'))
+files = glob.glob(osp.join(root,'*/catalog.json'))
+catalog_path=osp.join(root,'catalog.json')
+lock_path=catalog_path + '.lock'
+
 catalog = {}
 for f in files:
     catalog.update(json.load(open(f)))
-json.dump(catalog, open('catalog', 'w'), indent=1, separators=(',',':'))
+
+catalog_sorted=collections.OrderedDict(sorted(catalog.items(), reverse=True))
+
+l=lock.lock(lock_path)
+l.acquire()
+json.dump(catalog_sorted, open(catalog_path, 'w'), indent=1, separators=(',',':'))
+l.release()
+
+logging.info('Created catalog at %s',catalog_path)
     
 
--- src/lock.py
+++ src/lock.py
@@ -6,7 +6,7 @@ class lock():
 
     def __init__(self,path):
         self.lock_path = path
-        logging.info('Initializing on %s' % self.lock_path)
+        logging.info('Initializing lock on %s' % self.lock_path)
         self.lock_file=open(self.lock_path,'w',0)
         self.locked=False
 
