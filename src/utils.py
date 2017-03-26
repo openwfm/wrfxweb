d9951435b45917eb36f53c5b5aa3947697edcf33
Jan Mandel <jan.mandel@gmail.com>
2017-03-26 16:25:11 -0400

    adding update_nested_dict to utils.py

--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,47 @@ class lock():
         logging.info('Releasing lock on %s' % self.lock_path)
         fcntl.flock(self.lock_file,fcntl.LOCK_UN)
         self.locked=False
+
+def update_nested_dict(d,u,level=0):
+    """
+    Recursively update nested dictionary. Does not overwrite any values.
+    Identical key is allowed only if both values are dictionaries and the
+    update can continue recursively.
+
+    :param d: update: dictionary to be updated
+    :param u: input: dictionary with the update
+
+    :param level: internal, for error reporting only
+    :param key: internal, for error reporting only
+
+    Example:
+    from utils import update_nested_dict
+    d = {1: {2: 3}, 2: {4: 5}}
+    u = {1: {8: 9}, 3: {10: 11}}
+    update_nested_dict(d,u)
+    d
+    {1: {8: 9, 2: 3}, 2: {4: 5}, 3: {10: 11}}
+    update_nested_dict(d,u)
+    ValueError: update_nested_dict: level 1: values for common key 8 must be dictionaries 
+    """
+
+    # print ('update_nested_dict: level %s entering with d=%s u=%s' % (level,d,u))
+    if type(d) is not dict or type(u) is not dict:
+        raise ValueError ('update_nested_dict: level %s: both arguments must be dictionaries' % level)
+    for k in u.keys():
+        # print ('update_nested_dict: level %s found key %s in u' % (level,k))
+        if k in d:
+            # print ('update_nested_dict: level %s key %s in both u and d' % (level,k))
+            # print ('update_nested_dict: level %s recursive update in d=%s and u=%s' % (level,d,u))
+            if type(d[k]) is not dict or type(u[k]) is not dict:
+                raise ValueError ('update_nested_dict: level %s: values for common key %s must be dictionaries' % (level,k))
+            update_nested_dict(d[k],u[k],level+1)
+            # print ('update_nested_dict: level %s got updated d=%s' % (level,d))
+        else:
+            # print ('update_nested_dict: level %s key %s from u not found in d' % (level,k))
+            # print ('update_nested_dict: level %s adding item to d=%s from u=%s' % (level,d,u))
+            d[k]=u[k]
+            # print ('update_nested_dict: level %s got updated d=%s' % (level,d))
+    # print ('update_nested_dict: level %s exiting with updated d=%s' % (level,d))
+
+
