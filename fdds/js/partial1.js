d25b4331e86ffd90b26672d3a16230c008fb67fe
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2020-10-29 13:30:09 -0700

    Refactors catalogMenu
    Creates an HTMLComponent for the CatalogMenu that includes all js and html necessary to build the catalogmenu out of index.html and partial1.js

--- fdds/js/partial1.js
+++ fdds/js/partial1.js
@@ -72,65 +72,6 @@ map.on('overlayremove', function(e) {
 		displayed_colorbar = null;
 	}
 });
-
-
-$.when(
-
-  $.getJSON("simulations/catalog.json", function(data) {
-		catalog = data;
-		var list1 = $('#catalog-list-1');
-    var list2 = $('#catalog-list-2');
-    var list3 = $('#catalog-list-3');
-    $.each(data, function(cat_name) {
-      var cat_entry = data[cat_name];
-      var desc = cat_entry.description;
-      var from = cat_entry.from_utc;
-      var to = cat_entry.to_utc;
-      var job_id = cat_entry.job_id;
-      var kml_url = cat_entry.kml_url;
-      var kml_size = cat_entry.kml_size;
-      var zip_url = cat_entry.zip_url;
-      var zip_size = cat_entry.zip_size;
-      var load_cmd = '"handle_catalog_click(\'simulations/' + cat_entry.manifest_path + '\');"';
-		  var html = '<li class="catalog-entry" onclick=' + load_cmd + '><b>' 
-						   + desc + '</b><br/>' 
-                                                   + 'from: ' + from + '<br/>to: ' + to  + '<br/>';
-			if(job_id) {
-				html = html  + 'job id: ' + job_id + '<br/>';
-			}
-			html = html + '</li>';
-                        if(kml_url) {
-                                var mb = Math.round(10*kml_size/1048576.0)/10;
-				html = html + '<a href="' + kml_url + '" download>Download KMZ ' + mb.toString() +' MB</a><br/>' ;
-                        }
-                        if(zip_url) {
-                                var mb = Math.round(10*zip_size/1048576.0)/10;
-				html = html + '<a href="' + zip_url + '" download>Download ZIP ' + mb.toString() +' MB</a><br/>' ;
-                        }
-
-			if(desc.indexOf('GACC') >= 0) {
-        list2.append(html);
-      } else if(desc.indexOf('SAT') >= 0) {
-        list3.append(html);
-			} else {
-				list1.append(html);
-			}
-    });
-    
-  })).then(function() {
-
-    $('#select-dialog').dialog({ autoOpen: false });
-
-		var simid_ndx = window.location.hash.indexOf('sim_id');
-		var simid = window.location.hash.substring(simid_ndx+7);
-		if(simid_ndx >= 0 && simid in catalog) {
-			handle_catalog_click('simulations/' + catalog[simid].manifest_path);
-		} else {
-			open_catalog();
-		}
-
-});
-
 }
 
 
