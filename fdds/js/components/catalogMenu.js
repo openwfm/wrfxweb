731f4b718e65414946bde387708b3740a6207b30
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2020-11-05 14:09:22 -0800

    Cleans up code in getJSON portion of catalogMenu. Uses other functions to do heavy lifting.

--- fdds/js/components/catalogMenu.js
+++ fdds/js/components/catalogMenu.js
@@ -4,9 +4,9 @@
 class CatalogMenu extends HTMLElement {
     constructor() {
         super();
-        this.list1 = [];
-        this.list2 = [];
-        this.list3 = [];
+        this.firesList = [];
+        this.fuelMoistureList = [];
+        this.satelliteList = [];
         this.innerHTML = `
             <div class="catalog-menu">
                 <div id="menu-title" class="menu-title">
@@ -19,21 +19,15 @@ class CatalogMenu extends HTMLElement {
                 <div class="menu-columns" id="default-menu">
                     <div class="column">
                         <h3>Fires</h3>
-                        <ul id="catalog-list-1" class="catalog-list"> </ul>
+                        <ul id="catalog-fires" class="catalog-list"> </ul>
                     </div>
                     <div class="column">
                         <h3>Fuel moisture</h3>
-                        <ul id="catalog-list-2" class="catalog-list"> </ul>
+                        <ul id="catalog-fuel-moisture" class="catalog-list"> </ul>
                     </div>
                     <div class="column">
                         <h3>Satellite Data</h3>
-                        <ul id="catalog-list-3" class="catalog-list"> </ul>
-                    </div>
-                </div>
-                <div class="menu-columns" id="search-menu" style="display:none">
-                    <div class="column">
-                        <h3>Search Result</h3>
-                        <ul id="catalog-list-search" class="catalog-list"></ul>
+                        <ul id="catalog-satellite-data" class="catalog-list"> </ul>
                     </div>
                 </div>
             </div>
@@ -54,43 +48,19 @@ class CatalogMenu extends HTMLElement {
             e.stopPropagation();
         });
         menuSearch.oninput = (input) => this.searchCatalog(input);
-
-
         L.DomEvent.disableScrollPropagation(this.querySelector(".catalog-menu"));
         L.DomEvent.disableClickPropagation(this.querySelector(".catalog-menu"));
+        var parentComponent = this;
         
         $.getJSON("simulations/catalog.json", function(data) {
             catalog = data;
-            var list1 = $('#catalog-list-1');
-            var list2 = $('#catalog-list-2');
-            var list3 = $('#catalog-list-3');
+            const list1 = $('#catalog-fires');
+            const list2 = $('#catalog-fuel-moisture');
+            const list3 = $('#catalog-satellite-data');
             $.each(data, function(cat_name) {
                 var cat_entry = data[cat_name];
-                var desc = cat_entry.description;
-                var from = cat_entry.from_utc;
-                var to = cat_entry.to_utc;
-                var job_id = cat_entry.job_id;
-                var kml_url = cat_entry.kml_url;
-                var kml_size = cat_entry.kml_size;
-                var zip_url = cat_entry.zip_url;
-                var zip_size = cat_entry.zip_size;
-                var load_cmd = '"handle_catalog_click(\'simulations/' + cat_entry.manifest_path + '\');"';
-                var html = '<li class="catalog-entry" onclick=' + load_cmd + '><b>' 
-                            + desc + '</b><br/>' 
-                                                    + 'from: ' + from + '<br/>to: ' + to  + '<br/>';
-                if(job_id) {
-                    html = html  + 'job id: ' + job_id + '<br/>';
-                }
-                html = html + '</li>';
-                if(kml_url) {
-                    var mb = Math.round(10*kml_size/1048576.0)/10;
-                    html = html + '<a href="' + kml_url + '" download>Download KMZ ' + mb.toString() +' MB</a><br/>' ;
-                }
-                if(zip_url) {
-                    var mb = Math.round(10*zip_size/1048576.0)/10;
-                    html = html + '<a href="' + zip_url + '" download>Download ZIP ' + mb.toString() +' MB</a><br/>' ;
-                }
-
+                let desc = cat_entry.description;
+                var html = parentComponent.buildListItem(cat_entry);
                 if(desc.indexOf('GACC') >= 0) {
                     list2.append(html);
                 } else if(desc.indexOf('SAT') >= 0) {
@@ -102,12 +72,31 @@ class CatalogMenu extends HTMLElement {
         });
     }
 
+    buildListItem(cat_entry) {
+        var job_id = cat_entry.job_id;
+        var kml_url = cat_entry.kml_url;
+        var zip_url = cat_entry.zip_url;
+        var load_cmd = '"handle_catalog_click(\'simulations/' + cat_entry.manifest_path + '\');"';
+        var html = '<li class="catalog-entry" onclick=' + load_cmd + '><b>' 
+                    + cat_entry.description + '</b><br/>' 
+                                            + 'from: ' + cat_entry.from_utc + '<br/>to: ' + cat_entry.to_utc  + '<br/>';
+        if(job_id) {
+            html = html  + 'job id: ' + job_id + '<br/>';
+        }
+        html = html + '</li>';
+        if(kml_url) {
+            let mb = Math.round(10*cat_entry.kml_size/1048576.0)/10;
+            html = html + '<a href="' + kml_url + '" download>Download KMZ ' + mb.toString() +' MB</a><br/>' ;
+        }
+        if(zip_url) {
+            let mb = Math.round(10*cat_entry.zip_size/1048576.0)/10;
+            html = html + '<a href="' + zip_url + '" download>Download ZIP ' + mb.toString() +' MB</a><br/>' ;
+        }
+        return html;
+    }
+
     searchCatalog(input) {
         console.log(input);
-        const defaultMenu = this.querySelector("#default-menu");
-        const searchMenu = this.querySelector("#search-menu");
-        defaultMenu.style.display = "none";
-        searchMenu.style.display = "block";
     }
 
     dragElement(elmnt) {
