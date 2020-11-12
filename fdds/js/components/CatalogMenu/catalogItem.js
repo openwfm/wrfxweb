dd628a779ea1c9f54a4c84be0756e4a24eefa208
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2020-11-12 12:25:00 -0800

    reverting changes made for local development

--- fdds/js/components/CatalogMenu/catalogItem.js
+++ fdds/js/components/CatalogMenu/catalogItem.js
@@ -89,9 +89,7 @@ class CatalogItem extends HTMLElement {
         // show job description
         var catPath = path.substring(0,path.lastIndexOf("/") + 1) + "catalog.json";
         
-        //REVERT THIS BEFORE COMMITTING
-        $.getJSON(catPath.replaceAll(":", "_"), function(data) {
-        // $.getJSON(catPath, function(data) {
+        $.getJSON(catPath, function(data) {
             catalog = data;
             $.each(data, function(cat_name) {
                 var cat_entry = data[cat_name];
@@ -102,14 +100,10 @@ class CatalogItem extends HTMLElement {
             });
         });
 
-        // REVERT THIS
-        $.getJSON(path.replaceAll(":", "_"), function(selected_simulation) {
-        // $.getJSON(path, function(selected_simulation) {
+        $.getJSON(path, function(selected_simulation) {
             // store in global state
             rasters = selected_simulation;
-            // REVERT THIS
-            raster_base = "https://demo.openwfm.org/ch/" + path.substring(0, path.lastIndexOf('/') + 1);
-            // raster_base = path.substring(0, path.lastIndexOf('/') + 1);
+            raster_base = path.substring(0, path.lastIndexOf('/') + 1);
 
             // retrieve all domains
             domains = Object.keys(rasters);
