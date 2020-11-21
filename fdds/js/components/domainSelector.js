8a13f5c0cad538570e6ee26748c83c6afdcd1830
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2020-11-20 16:05:38 -0800

    Styles DomainSelector

--- fdds/js/components/domainSelector.js
+++ fdds/js/components/domainSelector.js
@@ -2,34 +2,28 @@ class DomainSelector extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
-            <div id="domain-selector" class="ui form">
-                <div class="ui grey label">
-                    <div id="domain-checkboxes" class="grouped inline fields">
-                    <div class="ui large label">Active domain</div><br/>
-                    </div>
-                </div>
+            <div id='domain-selector'>
+                <span id='domain-selector-label'>Active domain</span>
+                <div id='domain-checkboxes'></div>
             </div>
         `;
-    }
-
-    connectedCallback() {
     }
 
     buildDomains() {
         console.log("buildDomains");
         current_domain = domains[0];
         // update the domain radio buttons
-        // const domainCheckBoxes = this.querySelector('#domain-checkboxes');
-        // domainCheckBoxes.innerHTML = '';
-        $('#domain-checkboxes').empty();
-        $('#domain-checkboxes').append('<div class="ui large label">Active domain</div><br/>');
+        const domainCheckboxes = this.querySelector('#domain-checkboxes');
+        domainCheckboxes.innerHTML = '';
+        // $('#domain-checkboxes').empty();
         for(var dom in domains) {
             var dom_id = domains[dom];
             var checked = dom_id == '1' ? 'checked=yes' : '';
-            $('#domain-checkboxes').append('<div class="field"><div class="ui radio checkbox"><input type="radio" name="domains" id="' + dom_id + '"' + checked + ' onclick="setup_for_domain(\''+dom_id+'\');"/><label for="' + dom_id + '">' + dom_id + '</label></div></div>');
+            var domainCheckbox = '<div class="domain-checkbox"><input type="radio" name="domains" id="' + dom_id + '"' + checked + ' onclick="setup_for_domain(\''+dom_id+'\');"/><label for="' + dom_id + '">' + dom_id + '</label></div>';
+            domainCheckboxes.innerHTML += domainCheckbox;
         }
 
-        $('#domain-selector').show();
+        this.querySelector('#domain-selector').style.display = 'block';
         setup_for_domain(current_domain);
     }
 
