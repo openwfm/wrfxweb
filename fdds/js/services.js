2e59d7143c063295b1d4d3f4d71d38da8d27b3b2
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-01-05 11:08:46 -0800

    Reverting changes made for local development

--- fdds/js/services.js
+++ fdds/js/services.js
@@ -26,12 +26,10 @@ class Services {
 
     /** Service request for fetching a selected simulation from the menu. */
     getSimulation(path) {
-        fetch(path.replaceAll(":", "_")).then(response => response.json()).then(function(selectedSimulation) { 
-        // fetch(path).then(response => response.json()).then(function(selectedSimulation) { 
+        fetch(path).then(response => response.json()).then(function(selectedSimulation) { 
             // store in global state
             rasters = selectedSimulation;
-            // raster_base = path.substring(0, path.lastIndexOf('/') + 1);
-            raster_base = "https://demo.openwfm.org/sj/" + path.substring(0, path.lastIndexOf('/') + 1);
+            raster_base = path.substring(0, path.lastIndexOf('/') + 1);
             // retrieve all domains
             domainInstance.setValue(Object.keys(rasters));
         }).catch(error => {
