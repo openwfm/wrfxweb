f24d67fe48f9159e99f1a22afd7ad50ff37273c1
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-01-21 15:21:53 -0800

    Sets up mocking framework for controllers

--- fdds/js/tests/layerController.test.js
+++ fdds/js/tests/layerController.test.js
@@ -2,6 +2,14 @@ const {LayerController} = require("../components/layerController");
 const {L} = require("../leaflet/leaflet.js");
 jest.mock("../leaflet/leaflet.js");
 
+const controllers = require("../components/Controller.js");
+jest.mock('../components/Controller.js', () => ({
+    currentSimulation: ({
+        getValue: () => "test",
+        subscribe: () => {}
+    })
+}));
+
 describe('Setting up tests for layerController', () => {
     var layerController;
 
