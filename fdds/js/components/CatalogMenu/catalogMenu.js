e1f59f67a7b11df89dc675a8c25393176a91caf1
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-03-26 09:21:25 -0700

    cleans up catalogMenu of duplicate code

--- fdds/js/components/CatalogMenu/catalogMenu.js
+++ fdds/js/components/CatalogMenu/catalogMenu.js
@@ -2,13 +2,11 @@ import {dragElement} from '../../util.js';
 import {getCatalogEntries} from '../../services.js';
 import {CatalogItem} from './catalogItem.js';
 
-/**
- * A Component that builds the CatalogMenu. Can be added to html using <catalog-menu></catalog-menu> 
+/** A Component that builds the CatalogMenu. Can be added to html using <catalog-menu></catalog-menu> 
  * 
  * Includes three different columns for data related to fires, fuel moisture, and satellite data. 
  * Can be moved around by clicking the title bar, can be closed by clicking x in top right corner, and 
- * supports searching columns for data that matches a description.
- */
+ * supports searching columns for data that matches a description. */
 export class CatalogMenu extends HTMLElement {
     constructor() {
         super();
@@ -66,10 +64,8 @@ export class CatalogMenu extends HTMLElement {
         `;
     }
 
-    /**
-     * This function is called once the HTML Element has been added to the DOM. Add all the UI events and change some 
-     * presentation details based on whether on mobile. 
-     */
+    /** This function is called once the HTML Element has been added to the DOM. Add all the UI events and change some 
+     * presentation details based on whether on mobile. */
     connectedCallback() {
         const catalogMenu = this.querySelector('.catalog-menu');
         const clientWidth = document.body.clientWidth;
@@ -101,8 +97,7 @@ export class CatalogMenu extends HTMLElement {
     }
 
     /** Function that retrieves catalog Entries from services.js and builds a CatalogItem for each and adds it to the 
-     * appropriate category in the menu.
-     */
+     * appropriate category in the menu. */
     async buildMenu() {
         const urlParams = new URLSearchParams(window.location.search);
         const navJobId = urlParams.get('job_id');
@@ -130,40 +125,23 @@ export class CatalogMenu extends HTMLElement {
         }
     }
 
-    /** Called each time a character is entered into the search input. Clears each catalog column on the DOM,
-     * filters the stored array of catalog entries by its description for whether there is a match with the searched
-     * text. Builds <li> html for filtered catalog entries and adds them to the columns
-    */
+    /** Called each time a character is entered into the search input. filters the stored array of catalog entries by search text */
     searchCatalog(searchText, sortBy) {
-        const firesListDOM = this.querySelector('#catalog-fires');
-        const fuelMoistureListDOM = this.querySelector('#catalog-fuel-moisture');
-        const satelliteListDOM = this.querySelector('#catalog-satellite-data');
-        let catalogColumns = [[firesListDOM, this.firesList], [fuelMoistureListDOM, this.fuelMoistureList], [satelliteListDOM, this.satelliteList]];
-        var filterFunction = (catalogEntry) => {
+        const filterFunction = (catalogEntry) => {
             if (sortBy == 'original-order' || sortBy == 'description') return catalogEntry.description.toLowerCase().includes(searchText);
             if (sortBy.includes('start-date')) return catalogEntry.from_utc.toLowerCase().includes(searchText);
             if (sortBy.includes('end-date')) return catalogEntry.to_utc.toLowerCase().includes(searchText);
         }
-        catalogColumns.map(([listDOM, list]) => {
-            listDOM.innerHTML = '';
-            let filteredList = list.filter(filterFunction);
-            filteredList.map(catalogEntry => {
-                let matchedLI = new CatalogItem(catalogEntry, null);
-                listDOM.appendChild(matchedLI);
-            });
-        });
+        const createList = (list) => list.filter(filterFunction);
+        this.filterColumns(createList);
     }
 
     /** Function that sorts the lists in the menu. Takes @sortBy the selected category to sortBy and @reverseOrder indicating if 
-     * the order should be reversed.
-     */
+     * the order should be reversed. */
     sortBy(sortBy, reverseOrder) {
-        const firesListDOM = this.querySelector('#catalog-fires');
-        const fuelMoistureListDOM = this.querySelector('#catalog-fuel-moisture');
-        const satelliteListDOM = this.querySelector('#catalog-satellite-data');
         const catalogSearch = this.querySelector('#search-for');
         catalogSearch.value = "";
-        var sortingFunction = (listElem1, listElem2) => {
+        const sortingFunction = (listElem1, listElem2) => {
             let result = false;
             if (sortBy == "original-order") result = this.addOrder[listElem1.job_id] > this.addOrder[listElem2.job_id];
             if (sortBy == "description") result = listElem1.description > listElem2.description; 
@@ -172,20 +150,29 @@ export class CatalogMenu extends HTMLElement {
             if (reverseOrder) result = !result;
             return result ? 1 : -1;
         }
+        const createList = (list) => list.sort(sortingFunction);
+        this.filterColumns(createList);
+    }
+
+    /** Clears each catalog column on the DOM, filters the stored array of catalog entries by 
+     * provided function. Builds <li> html for filtered catalog entries and adds them to the columns */
+    filterColumns(createList) {
+        const firesListDOM = this.querySelector('#catalog-fires');
+        const fuelMoistureListDOM = this.querySelector('#catalog-fuel-moisture');
+        const satelliteListDOM = this.querySelector('#catalog-satellite-data');
         let catalogColumns = [[firesListDOM, this.firesList], [fuelMoistureListDOM, this.fuelMoistureList], [satelliteListDOM, this.satelliteList]];
-        catalogColumns.map(([listDOM, list]) => {
+        for (const [listDOM, list] of catalogColumns) {
             listDOM.innerHTML = '';
-            let filteredList = list.sort(sortingFunction);
-            filteredList.map(catalogEntry => {
+            let newList = createList(list);
+            for (var catalogEntry of newList) {
                 let newLI = new CatalogItem(catalogEntry, null);
                 listDOM.append(newLI);
-            });
-        });
+            }
+        }
     }
 
     /** Function used only in mobile versions. Mobile shows only one column at a time and this function is called when a user switches between columns. 
-     * Hides all columns and then shows the selected column.
-     */
+     * Hides all columns and then shows the selected column. */
     selectCategory(selection) {
         const firesListDOM = this.querySelector('#fires-column');
         firesListDOM.style.display = "none";
