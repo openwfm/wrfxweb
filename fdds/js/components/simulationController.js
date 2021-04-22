867f899b63fa071e0d383a23240fd9b83f98debc
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-04-22 12:00:33 -0700

    Removes unused code in simulationController (all code relating to preloading images

--- fdds/js/components/simulationController.js
+++ fdds/js/components/simulationController.js
@@ -36,7 +36,6 @@ export class SimulationController extends HTMLElement {
             </div>
         `;
 
-        this.preloaded = {}; // dictionary containing information on what frames have been preloaded for which rasters/layers
         this.currentSimulation = "";
         this.currentFrame = 0;
         this.frameTotal = 1;
@@ -65,12 +64,10 @@ export class SimulationController extends HTMLElement {
         let percentage = this.currentFrame / this.frameTotal;
         this.currentFrame = Math.floor((sorted_timestamps.getValue().length) * percentage);
         if (this.currentSimulation != currentSimulation.getValue()) {
-            this.preloaded = {};
             this.currentSimulation = currentSimulation.getValue();
             percentage = 0;
             this.currentFrame = 0;
         }
-        // this.preloadVariables(this.currentFrame, 8);
         this.setupForTime(this.currentFrame);
         this.frameTotal = sorted_timestamps.getValue().length;
         this.querySelector('#slider-head').style.left = Math.floor(percentage * 92) + "%";
@@ -122,14 +119,8 @@ export class SimulationController extends HTMLElement {
             return;
         }
         let nextFrame = (this.currentFrame + 1) % sorted_timestamps.getValue().length;
-        if(this.frameReady(nextFrame)) {
-            this.currentFrame = nextFrame;
-            this.updateSlider();
-        } else {
-            // if the next frame is not ready, preload further and wait longer
-            window.setTimeout(() => this.nextFrame(recursionDepth - 1), 500);
-            this.preloadVariables(nextFrame, 8);
-        }
+        this.currentFrame = nextFrame;
+        this.updateSlider();
     }
 
     /** Moves one frame to the left. */
@@ -137,71 +128,16 @@ export class SimulationController extends HTMLElement {
         if (recursionDepth == 0) return;
         let prevFrame = (this.currentFrame - 1) % sorted_timestamps.getValue().length;
         if (prevFrame < 0) prevFrame += sorted_timestamps.getValue().length;
-        if(this.frameReady(prevFrame)) {
-            this.currentFrame = prevFrame;
-            this.updateSlider();
-        } else {
-            window.setTimeout(() => this.prevFrame(recursionDepth - 1), 500);
-            this.preloadVariables(Math.max(prevFrame - 7, 0), 8);
-        }
+        this.currentFrame = prevFrame;
+        this.updateSlider();
     }
 
-    /** Returns boolean indicating if frame_ndx has been loaded */
-    frameReady(frame_ndx) {
-    // for all layers currently displayed
-        return true;
-        // for(var key of overlayOrder) {
-        //     // if the current frame is not preloaded yet
-        //     var currDomain = currentDomain.getValue();
-        //     if(this.preloaded[key] == null) return false;
-        //     if(this.preloaded[key][currDomain] == null) return false;
-        //     if(!(frame_ndx in this.preloaded[key][currDomain])) return false;
-        //     // check if the raster has a colorbar
-        //     var cb_key = key + '_cb';
-        //     if(cb_key in this.preloaded && !(frame_ndx in this.preloaded[cb_key])) return false;
-        // }
-        // return true;
-    }
-    
     // this function should assume that the correct layers are already displayed
     setupForTime(frame_ndx) {
         var timestamp = sorted_timestamps.getValue()[frame_ndx];
         // set current time
         document.querySelector('#timestamp').innerText = utcToLocal(timestamp);
         current_timestamp.setValue(timestamp);
-    }
-
-    /* Code handling auxiliary tasks */
-    preloadVariables(frame, preload_count) {
-        var rasters_dom = rasters.getValue()[currentDomain.getValue()];
-        var n_rasters = Object.keys(rasters_dom).length;
-        preload_count = Math.min(preload_count, n_rasters);
-        for (var counter=0; counter < preload_count; counter++) {
-            var i = (frame + counter) % n_rasters;
-            var timestamp = sorted_timestamps.getValue()[i];
-            for (var var_name of overlayOrder) {
-                // it could happen that a timestamp is missing the variable
-                if (var_name in rasters_dom[timestamp]) {
-                    // have we already preloaded this variable? If not indicate nothing is preloaded.
-                    var currDomain = currentDomain.getValue();
-                    if (!(var_name in this.preloaded)) this.preloaded[var_name] = {};
-                    if (!(currDomain in this.preloaded[var_name])) this.preloaded[var_name][currDomain] = {};
-                    if (!(i in this.preloaded[var_name][currDomain])) {
-                        var var_info = rasters_dom[timestamp][var_name];
-                        var img = new Image();
-                        img.onload = this.preloaded[var_name][currDomain][i] = img;
-                        img.src = raster_base.getValue() + var_info.raster;
-                        if ('colorbar' in var_info) {
-                            var cb_key = var_name + '_cb';
-                            if(!(cb_key in this.preloaded)) this.preloaded[cb_key] = {};
-                            var img = new Image();
-                            img.onload = this.preloaded[cb_key][i] = img;
-                            img.src = raster_base.getValue() + var_info.colorbar;
-                        }
-                    }
-                }
-            }
-        }
     }
 
     /** Called when slider head is dragged. As dragged, calculates distance dragged and updates
@@ -228,7 +164,6 @@ export class SimulationController extends HTMLElement {
 
             let newFrame = originalFrame + diff;
             this.currentFrame = Math.max(Math.min(sorted_timestamps.getValue().length-1, newFrame), 0);
-            // this.preloadVariables(Math.max(this.currentFrame - 4, 0), 8);
             this.updateSlider();
           }
     }
