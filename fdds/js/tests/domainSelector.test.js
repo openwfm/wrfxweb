e21ffa0cc87b51497008433e3f3ca936e6816d87
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-05-26 10:46:01 -0700

    Fixes broken domainSelector test

--- fdds/js/tests/domainSelector.test.js
+++ fdds/js/tests/domainSelector.test.js
@@ -1,54 +1,54 @@
-const {DomainSelector} = require("../components/domainSelector");
+const { DomainSelector } = require('../components/domainSelector');
 
-const controllers = require("../components/Controller.js");
-jest.mock('../components/Controller.js', () => ({
-    domainInstance: ({
-        subscribe: jest.fn(),
-        getValue: () => [1, 2]
-    }),
-    currentDomain: ({
-        getValue: jest.fn(),
-        setValue: jest.fn()
-    }),
-    sorted_timestamps: ({
-        getValue: jest.fn(),
-        setValue: jest.fn()
-    }),
-    rasters: ({
-        getValue: () => ({
+const util = require('../util.js');
+jest.mock('../util.js', () => ({
+    simVars: ({
+        sortedTimestamps: [],
+        rasters: ({
             1: {
                 "2020": {"raster": {}}
             },
             2: {
                 "2020": {"raster": {}},
                 "2021": {"raster": {}}
-            }
+            } 
         })
-    }),
+    })
+}));
+
+const controller = require('../components/Controller.js');
+jest.mock('../components/Controller.js', () => ({
+    controllers: ({
+        domainInstance: ({
+            subscribe: jest.fn(),
+            getValue: () => [1, 2]
+        }),
+        currentDomain: ({
+            getValue: jest.fn(),
+            setValue: jest.fn()
+        }),
+    })
 }));
 
 describe('Domain Selector Tests', () => {
     var domainSelector;
-    var sorted_timestamps;
     var currentDomain;
 
     beforeEach(async () => {
-        controllers.sorted_timestamps.setValue = (newSortedTimestamps) => sorted_timestamps = newSortedTimestamps;
-        controllers.sorted_timestamps.getValue = () => sorted_timestamps;
-        controllers.currentDomain.setValue = (newDomain) => currentDomain = newDomain;
+        controller.controllers.currentDomain.setValue = (newDomain) => currentDomain = newDomain;
         domainSelector = await document.body.appendChild(new DomainSelector());
     });
 
     test('Tests setUpForDomain', () => {
         domainSelector.setUpForDomain(1);
-        expect(sorted_timestamps).toEqual(["2020"]);
+        expect(util.simVars.sortedTimestamps).toEqual(["2020"]);
         expect(currentDomain).toEqual(1);
     });
 
     test('Tests calling setUpForDomain twice', () => {
         domainSelector.setUpForDomain(1);
         domainSelector.setUpForDomain(2);
-        expect(sorted_timestamps).toEqual(["2020", "2021"]);
+        expect(util.simVars.sortedTimestamps).toEqual(["2020", "2021"]);
         expect(currentDomain).toEqual(2);
     });
 });
