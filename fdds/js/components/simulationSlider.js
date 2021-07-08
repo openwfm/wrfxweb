f32a4a8232aa83693a2f1bd17e78ff13a1a4de14
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-07-08 15:02:10 -0700

    Reformats the way the progress bar is drawn. Now makes sure to fill remaining space between the two sliders

--- fdds/js/components/simulationSlider.js
+++ fdds/js/components/simulationSlider.js
@@ -6,6 +6,7 @@ import { simVars } from '../simVars.js';
 export class SimulationSlider extends Slider {
     constructor() {
         super(340, simVars.sortedTimestamps.length - 1);
+        this.progressWidth = 0;
     }
 
     connectedCallback() {
@@ -22,11 +23,13 @@ export class SimulationSlider extends Slider {
             this.updateStartLocation();
             this.updateEndLocation();
             updateSlider();
+            this.updateProgressWidth();
         }, controllerEvents.all);
 
         controllers.currentTimestamp.subscribe(updateSlider);
         controllers.loadingProgress.subscribe(() => {
-            this.setLoadProgress();
+            var progress = controllers.loadingProgress.value;
+            this.setLoadProgress(progress);
         });
 
         const slider = this.shadowRoot.querySelector('#slider');
@@ -105,9 +108,8 @@ export class SimulationSlider extends Slider {
         this.configureEndSetter();
     }
 
-    setLoadProgress() {
-        var progress = controllers.loadingProgress.getValue();
-        var progressWidth = progress*this.sliderWidth;
+    setLoadProgress(progress) {
+        var progressWidth = progress*this.progressWidth + 2;
 
         const progressBar = this.shadowRoot.querySelector('#slider-progress'); 
         progressBar.style.display = 'block';
@@ -122,6 +124,13 @@ export class SimulationSlider extends Slider {
         var left = Math.floor((startIndex / (simVars.sortedTimestamps.length - 1)) * this.sliderWidth * .95);
 
         progressBar.style.left = left + 'px';
+    }
+
+    getLeftOfDate(date) {
+        var index = simVars.sortedTimestamps.indexOf(date);
+        var left = Math.floor((index / (simVars.sortedTimestamps.length - 1)) * this.sliderWidth * .95);
+
+        return left;
     }
 
     setSliderMarkerInfo(timeStamp) {
@@ -169,6 +178,7 @@ export class SimulationSlider extends Slider {
 
         controllers.startDate.subscribe(() => {
             this.updateStartLocation();
+            this.updateProgressWidth();
         });
     }
 
@@ -211,25 +221,55 @@ export class SimulationSlider extends Slider {
 
         controllers.endDate.subscribe(() => {
             this.updateEndLocation();
+            this.updateProgressWidth();
         });
+    }
+
+    updateProgressWidth() {
+        var startLeft = this.getStartLeft();
+        var endLeft = this.getEndLeft();
+        var totalWidth = endLeft - (startLeft + 4);
+        // var startLeft = this.getLeftOfDate(controllers.startDate.getValue());
+        // const startBar = this.shadowRoot.querySelector('#slider-start');
+        // const endBar = this.shadowRoot.querySelector('#slider-end');
+        // var startRect = startBar.getBoundingClientRect();
+        // var endRect = endBar.getBoundingClientRect();
+        // var totalWidth = endRect.left - (startRect.left + startRect.width);
+        this.progressWidth = totalWidth;
     }
 
     updateStartLocation() {
         const sliderStart = this.shadowRoot.querySelector('#slider-start');
 
-        var startDate = controllers.startDate.getValue();
-        var startIndex = simVars.sortedTimestamps.indexOf(startDate);
-        var left = Math.floor((startIndex / (simVars.sortedTimestamps.length - 1)) * this.sliderWidth * .95) - 2;
+        // var startDate = controllers.startDate.getValue();
+        // var startIndex = simVars.sortedTimestamps.indexOf(startDate);
+        // var left = Math.floor((startIndex / (simVars.sortedTimestamps.length - 1)) * this.sliderWidth * .95) - 2;
+        var left = this.getStartLeft();
 
         sliderStart.style.left = left + 'px';
+    }
+
+    getStartLeft() {
+        var startDate = controllers.startDate.getValue();
+        // var startIndex = simVars.sortedTimestamps.indexOf(startDate);
+        var left = this.getLeftOfDate(startDate);
+        // var left = Math.floor((startIndex / (simVars.sortedTimestamps.length - 1)) * this.sliderWidth * .95) - 2;
+        return left - 2;
+    }
+
+    getEndLeft() {
+        var endDate = controllers.endDate.value;
+        var left = this.getLeftOfDate(endDate);
+        return left + 14;
     }
 
     updateEndLocation() {
         const sliderEnd = this.shadowRoot.querySelector('#slider-end');
 
-        var endDate = controllers.endDate.getValue();
-        var endIndex = simVars.sortedTimestamps.indexOf(endDate);
-        let left = Math.floor((endIndex / (simVars.sortedTimestamps.length - 1)) * (this.sliderWidth*.95) + 14);
+        // var endDate = controllers.endDate.getValue();
+        // var endIndex = simVars.sortedTimestamps.indexOf(endDate);
+        // let left = Math.floor((endIndex / (simVars.sortedTimestamps.length - 1)) * (this.sliderWidth*.95) + 14);
+        var left = this.getEndLeft();
 
         sliderEnd.style.left = left + 'px';
     }
