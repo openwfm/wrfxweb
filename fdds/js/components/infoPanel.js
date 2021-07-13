23bde4bb5da34a8a4cbdddf7b6bbb50fc0763a89
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-07-13 10:49:00 -0700

    Committing new components

---
+++ fdds/js/components/infoPanel.js
@@ -0,0 +1,42 @@
+class InfoPanel extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `
+            <link rel='stylesheet' href='css/infoPanel.css'/>
+            <div id='infoPanelContainer'>
+                <button id='infoButton'>
+                    <img src='icons/info_black_24dp.svg'></img>
+                </button></div>
+                <div id='infoPanel'>
+                    <span id='closeInfoPanel'>x</span>
+                </div>
+            </div>
+        `;
+    }
+
+    connectedCallback() {
+        const infoButton = this.querySelector('#infoButton');
+        const infoPanel = this.querySelector('#infoPanel');
+        const closePanel = this.querySelector('#closeInfoPanel');
+        L.DomEvent.disableClickPropagation(infoButton);
+
+        infoButton.onclick = () => {
+            if (infoPanel.classList.contains('clicked')) {
+                infoPanel.classList.remove('clicked');
+            } else {
+                infoPanel.classList.add('clicked');
+            }
+        }
+        infoButton.onmouseover = () => {
+            infoPanel.classList.add('hovered');
+        }
+        infoButton.onmouseout = () => {
+            infoPanel.classList.remove('hovered');
+        }
+        closePanel.onclick = () => {
+            infoPanel.style.display = 'none';
+        }
+    }
+}
+
+window.customElements.define('info-panel', InfoPanel);
