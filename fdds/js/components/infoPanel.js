a7e79383167a775c1fb83a74b95537c3b71036d6
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-07-13 16:43:47 -0700

    Formats the info document and updates the styling a bit

--- fdds/js/components/infoPanel.js
+++ fdds/js/components/infoPanel.js
@@ -9,6 +9,28 @@ class InfoPanel extends HTMLElement {
                 </button></div>
                 <div id='infoPanel'>
                     <span id='closeInfoPanel'>x</span>
+                    <h2>Feature List and Components</h2>
+                    <h3>Catalog Menu</h3>
+                        <p>The Catalog Menu shows all available simulations that can 
+                        be shown on the current account. </p>
+                        <p>To access the menu click the Catalog button in the 
+                        top left of the screen</p>
+                        <h4>Searching and Ordering Catalog<h4>
+                    <div id='break' style='width: 100%; height: 1px; background: #5d5d5d'></div>
+                    <h3>Layer Controller</h3>
+                        <h4>Layer Selection</h4>
+                        <h4>Top Layer Opacity</h4>
+                        <h4>TimeSeries Markers</h4>
+                        <h4>TimeSeries Chart</h4>
+                    <div id='break' style='width: 100%; height: 1px; background: #5d5d5d'></div>
+                    <h3>Simulation Controller</h3>
+                        <h4>Simulation Start and Stop Times</h4>
+                        <p>Description of simulation start and stop times</p>
+                        <p>How to use simulation start and stop times</p>
+                    <div id='break' style='width: 100%; height: 1px; background: #5d5d5d'></div>
+                    <h3>Domain Selector</h3>
+                    <div id='break' style='width: 100%; height: 1px; background: #5d5d5d'></div>
+                    <h3>Url Navigation</h3>
                 </div>
             </div>
         `;
@@ -19,6 +41,7 @@ class InfoPanel extends HTMLElement {
         const infoPanel = this.querySelector('#infoPanel');
         const closePanel = this.querySelector('#closeInfoPanel');
         L.DomEvent.disableClickPropagation(infoButton);
+        L.DomEvent.disableClickPropagation(infoPanel);
 
         infoButton.onclick = () => {
             if (infoPanel.classList.contains('clicked')) {
