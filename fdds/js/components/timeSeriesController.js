5d23be3b6374182720830fa072e9b1ac25097d6b
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-07-22 15:06:57 -0700

    Fixes styling of buttons in timeSeriesChart so they don't offset the chart and hide the configurables from view.

--- fdds/js/components/timeSeriesController.js
+++ fdds/js/components/timeSeriesController.js
@@ -63,15 +63,15 @@ export class TimeSeriesController extends LayerController {
         var endDate = controllers.endDate.getValue();
         this.timeSeriesButton.setEndDate(endDate);
 
-        // this.handleOverlayadd('T2');
-        // const timeSeriesChart = document.querySelector('timeseries-chart');
-        // simVars.displayedColorbar = 'test';
-        // var dataset = [];
-        // dataset.push({label: 'test', latLon: {lat: 1, lng: 0}, rgb: [0, 0, 0], 
-        //                  dataset: {'2020-10-15 17:00:00': 12, '2020-10-15 18:00:00': 19, '2020-10-15 19:00:00': 20}});
-        // dataset.push({label: 'test2', latLon: {lat: 1, lng: 0}, rgb: [0, 180, 0],
-        //                     dataset: {'2020-10-15 17:00:00': 18, '2020-10-15 18:00:00': 12, '2020-10-15 19:00:00': 10}});
-        // timeSeriesChart.populateChart(dataset);
+        this.handleOverlayadd('T2');
+        const timeSeriesChart = document.querySelector('timeseries-chart');
+        simVars.displayedColorbar = 'test';
+        var dataset = [];
+        dataset.push({label: 'test', latLon: {lat: 1, lng: 0}, rgb: [0, 0, 0], 
+                         dataset: {'2020-10-15 17:00:00': 12, '2020-10-15 18:00:00': 19, '2020-10-15 19:00:00': 20}});
+        dataset.push({label: 'test2', latLon: {lat: 1, lng: 0}, rgb: [0, 180, 0],
+                            dataset: {'2020-10-15 17:00:00': 18, '2020-10-15 18:00:00': 12, '2020-10-15 19:00:00': 10}});
+        timeSeriesChart.populateChart(dataset);
     }
 
     /** If a colorbar is included in the new added layer, need to set it up for timeSeries:
