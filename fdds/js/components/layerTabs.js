864901caf0c7334b34cdaad57ea4046d368421bb
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-08-25 16:04:52 -0700

    FEATURE EXTENSION: Adds ability to create tabs on top of the LayerSelector to add additional simulations to the screen.

--- fdds/js/components/layerTabs.js
+++ fdds/js/components/layerTabs.js
@@ -3,10 +3,51 @@ export class LayerTabs extends HTMLElement {
         super();
         this.innerHTML = `
             <div id='added-simulations'>
-                <div class='tab' id='add-simulation'>+</div>
+                <div class='tab' id='add-simulation'>
+                    <div class='interactive-button innerTab'>+</div>
+                </div>
             </div>
         `;
+
+        this.activeTab = null;
     }
+
+    makeNewTab(id) {
+        const addedSimulations = this.querySelector('#added-simulations');
+        const addSimulation = this.querySelector('#add-simulation');
+
+        const newTab = document.createElement('div');
+        newTab.classList.add('tab');
+        const innerTab = document.createElement('div');
+        innerTab.classList.add('interactive-button');
+        innerTab.classList.add('innerTab');
+        innerTab.innerText = id;
+        newTab.appendChild(innerTab);
+
+        addedSimulations.insertBefore(newTab, addSimulation)
+        newTab.onclick = () => {
+            this.switchActiveTab(newTab);
+        }
+    }
+
+    connectedCallback() {
+        const addSimulation = this.querySelector('#add-simulation');
+        addSimulation.onclick = () => {
+            this.makeNewTab('1');
+        }
+    }
+
+    switchActiveTab(newTab) {
+        if (this.activeTab == newTab) {
+            return;
+        }
+        if (this.activeTab) {
+            this.activeTab.classList.remove('active');
+        }
+        this.activeTab = newTab;
+        this.activeTab.classList.add('active');
+    }
+
 }
 
 window.customElements.define('layer-tabs', LayerTabs);
