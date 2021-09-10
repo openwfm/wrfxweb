67dd80d9323d07d68b6d6047a21c34512cfe170d
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-09-09 21:00:22 -0700

    BUG FIX: Object URLs cannot be loaded from worker.

--- fdds/imageLoadingWorker.js
+++ fdds/imageLoadingWorker.js
@@ -10,13 +10,9 @@ self.addEventListener('message', async event => {
 
     const response = await fetch(imageURL);
     const blob = await response.blob();
-    var objectURL = null;
-    if (blob.size > 0) {
-        objectURL = URL.createObjectURL(blob);
-    }
     self.postMessage({
         imageURL: imageURL, 
-        objectURL: objectURL,
+        blob: blob,
         timeStamp: timeStamp,
         layerName: layerName,
         layerDomain: layerDomain,
