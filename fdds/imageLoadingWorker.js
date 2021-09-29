9cfd8279bc5e6e6f295ad781d94b2e9512f80f9d
Chase Porter <chaseporter@Chases-MacBook-Pro.local>
2021-09-29 14:44:14 -0700

    REFACTOR imageLoadingWorker and threadManager for vars into lets

--- fdds/imageLoadingWorker.js
+++ fdds/imageLoadingWorker.js
@@ -3,7 +3,7 @@
 self.addEventListener('message', async event => {
     const imageInfo = event.data;
 
-    var imageURL = imageInfo.imageURL;
+    let imageURL = imageInfo.imageURL;
     const response = await fetch(imageURL);
     const blob = await response.blob();
     imageInfo.blob = blob;
