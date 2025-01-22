2728b2abc2639e4c53bc3988b190c61dcf127aed
Chase Porter <chaseporter15@gmail.com>
2025-01-22 10:34:16 -0800

    Rewrites the google auth code to follow the documentation from google

--- python/clientServer/routes/login.py
+++ python/clientServer/routes/login.py
@@ -1,17 +1,22 @@
 from ..app import app, db
 from ..models.User import User
-from ..serverKeys import CLIENT_ID, CLIENT_SECRET, OAUTH_REDIRECT_URI, ENVIRONMENT
+from ..serverKeys import (
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CLIENT_SECRETS_FILE,
+    OAUTH_SCOPES,
+)
 from ..services import UserServices as UserServices
 
 from flask import request, redirect, url_for, session, render_template, flash, abort
 from flask_login import login_user, logout_user, current_user, LoginManager
 from functools import wraps
-from urllib.parse import urlencode
 
-import secrets
 import datetime
-import requests
 
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
 
 provider_data = {
     "client_id": CLIENT_ID,
@@ -43,30 +48,35 @@ def login_required(f):
     return wrapper
 
 
+# https://developers.google.com/identity/protocols/oauth2/web-server#python_1
 @app.route("/login/google")
 def google_login():
     if not current_user.is_anonymous:
         return redirect(url_for("index"))
 
-    # generate a random string for the state parameter
-    session["oauth2_state"] = secrets.token_urlsafe(16)
-
     # create a query string with all the OAuth2 parameters
     redirect_url = url_for("authorize_google", _external=True)
-    if ENVIRONMENT == "production":
-        redirect_url = f"{OAUTH_REDIRECT_URI}/authorize/google"
-    qs = urlencode(
-        {
-            "client_id": provider_data["client_id"],
-            "redirect_uri": redirect_url,
-            "response_type": "code",
-            "scope": " ".join(provider_data["scopes"]),
-            "state": session["oauth2_state"],
-        }
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=OAUTH_SCOPES
+    )
+    flow.redirect_uri = redirect_url
+
+    authorization_url, state = flow.authorization_url(
+        # Recommended, enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type="offline",
+        # Optional, enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes="true",
+        # Optional, set prompt to 'consent' will prompt the user for consent
+        # prompt="consent",
     )
 
+    # generate a random string for the state parameter
+    session["oauth2_state"] = state
+
     # redirect the user to the OAuth2 provider authorization URL
-    return redirect(provider_data["authorize_url"] + "?" + qs)
+    return redirect(authorization_url)
 
 
 @app.route("/authorize/google", methods=["GET"])
@@ -75,72 +85,28 @@ def authorize_google():
     if not current_user.is_anonymous:
         return redirect(url_for("index"))
 
-    # if there was an authentication error, flash the error messages and exit
-    if "error" in request.args:
-        for k, v in request.args.items():
-            if k.startswith("error"):
-                flash(f"{k}: {v}")
-        return redirect(url_for("index"))
+    state = session["oauth2_state"]
 
-    # make sure that the state parameter matches the one we created in the
-    # authorization request
-    if request.args["state"] != session.get("oauth2_state"):
-        app.logger.info(
-            f"[LoginError] request state doesn't match one created in authorization request {time_now}"
-        )
-        if request.args["state"] == None:
-            app.logger.info(f"[LoginError] no session state present")
-        if session.get("oauth2_state") == None:
-            app.logger.info(f"[LoginError] no request state present")
-
-        abort(401)
-
-    # make sure that the authorization code is present
-    if "code" not in request.args:
-        app.logger.info(f"[LoginError] no authorization code in request {time_now}")
-        abort(401)
-
-    # exchange the authorization code for an access token
-    response = requests.post(
-        provider_data["token_url"],
-        data={
-            "client_id": provider_data["client_id"],
-            "client_secret": provider_data["client_secret"],
-            "code": request.args["code"],
-            "grant_type": "authorization_code",
-            "redirect_uri": url_for("authorize_google", _external=True),
-        },
-        headers={"Accept": "application/json"},
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=OAUTH_SCOPES, state=state
     )
-    if response.status_code != 200:
-        app.logger.info(
-            f"[LoginError] error status code when getting oauth2_token {response.status_code}: {response.json()} {time_now}"
-        )
-        abort(401)
-    oauth2_token = response.json().get("access_token")
-    if not oauth2_token:
-        app.logger.info(f"[LoginError] no oauth2_token: {response.json()} {time_now}")
-        abort(401)
+    request_url = url_for("authorize_google", _external=True)
+    flow.redirect_uri = request_url
 
-    # use the access token to get the user's email address
-    response = requests.get(
-        provider_data["userinfo"]["url"],
-        headers={
-            "Authorization": "Bearer " + oauth2_token,
-            "Accept": "application/json",
-        },
-    )
-    if response.status_code != 200:
-        app.logger.info(
-            f"[LoginError] error status code when retrieving user email {response.status_code}: {response.json()} {time_now}"
-        )
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
 
-        abort(401)
-    email = provider_data["userinfo"]["email"](response.json())
+    # Store credentials in the session.
+    credentials = flow.credentials
+    credentials = credentials
+
+    user_info_service = build("oauth2", "v2", credentials=credentials)
+    user_info = user_info_service.userinfo().get().execute()
+    email = user_info["email"]
 
     # find or create the user in the database
     user = UserServices.find_or_create(email)
-    time_now = datetime.datetime.now().strftime("%Y-%m-%d:%H-%M-%S")
 
     app.logger.info(f"[Login] {user.email} {time_now}")
 
--- python/clientServer/serverKeys.py
+++ python/clientServer/serverKeys.py
@@ -10,3 +10,5 @@ OAUTH_REDIRECT_URI = os.getenv("OAUTH_REDIRECT_URI")
 API_URL = os.getenv("API_URL")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 LOG_FILE = os.getenv("LOG_FILE")
+CLIENT_SECRETS_FILE = os.getenv("CLIENT_SECRETS_FILE")
+OAUTH_SCOPES = ["openid", "https://www.googleapis.com/auth/userinfo.email"]
