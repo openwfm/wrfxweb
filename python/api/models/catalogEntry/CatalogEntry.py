d2039794db7f73d5653ffce8adb16842781fa81c
Chase Porter <chaseporter15@gmail.com>
2025-03-31 23:17:04 -0700

    fixes bug serving from wrong directory

--- python/api/models/catalogEntry/CatalogEntry.py
+++ python/api/models/catalogEntry/CatalogEntry.py
@@ -1,19 +1,17 @@
 from api.session import db_session
 from api.models.User import User
 from api.models.catalogEntry.CatalogEntryDbModel import CatalogEntryDbModel
-from api.apiKeys import SIMULATIONS_FOLDER
+from api.apiKeys import FLASK_SIMULATIONS_FOLDER
 import api.encryption as encryption
 
 
 class CatalogEntry(CatalogEntryDbModel):
     def entry_path(self):
-        entry_path = (
-            f"{SIMULATIONS_FOLDER}/{encryption.decrypt_searchable_data(self.job_id)}"
-        )
+        entry_path = f"{FLASK_SIMULATIONS_FOLDER}/{encryption.decrypt_searchable_data(self.job_id)}"
         return entry_path
 
     def entry_manifest_path(self):
-        manifest_path = f"{SIMULATIONS_FOLDER}/{encryption.decrypt_searchable_data(self.manifest_path)}"
+        manifest_path = f"{FLASK_SIMULATIONS_FOLDER}/{encryption.decrypt_searchable_data(self.manifest_path)}"
         return manifest_path
 
     def uploader(self):
@@ -22,10 +20,10 @@ class CatalogEntry(CatalogEntryDbModel):
         return User.query.get(self.uploader_id)
 
     def directory(self):
-        return SIMULATIONS_FOLDER
+        return FLASK_SIMULATIONS_FOLDER
 
     def entry_directory(self):
-        return f"{SIMULATIONS_FOLDER}/{encryption.decrypt_searchable_data(self.job_id)}"
+        return f"{FLASK_SIMULATIONS_FOLDER}/{encryption.decrypt_searchable_data(self.job_id)}"
 
     def destroy(self):
         db_session.delete(self)
