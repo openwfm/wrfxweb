9d7fe10e97cb57781c354c4a47f81b8bdfd41fd0
Catherine <c.n@pdx.edu>
2025-07-28 13:04:15 -0700

    Media now loads in the component :D

--- fdds/js/components/satelliteDataPanel.js
+++ fdds/js/components/satelliteDataPanel.js
@@ -4,7 +4,7 @@ export class satelliteDataPanel extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
-            <div id="satellite-data-panel" class="feature-controller hidden" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <div id="satellite-data-panel-container" class="feature-controller hidden" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
                 <div id="satellite-data-panel-header">
                     <span id="media-title">Media Viewer</span>
                     <button id="close-media" class="interactive-button close-panel">×</button>
@@ -18,11 +18,15 @@ export class satelliteDataPanel extends HTMLElement {
     }
 
     connectedCallback() {
-        const panel = this.querySelector('#satellite-data-panel');
+        const panel = this.querySelector('#satellite-data-panel-container');
         const closeBtn = this.querySelector('#close-media');
 
         // Make panel draggable 
-        dragElement(panel, "#satellite-data-panel");
+        if (panel) {
+            dragElement(panel);
+        } else {
+            console.error('Panel element not found');
+        }
         L.DomEvent.disableClickPropagation(panel);
         L.DomEvent.disableScrollPropagation(panel);
 
@@ -30,21 +34,32 @@ export class satelliteDataPanel extends HTMLElement {
         closeBtn.onclick = () => {
             this.hide();
         };
+
+        const placeholder_media = `
+        <img class="satellite-img" src="https://placecats.com/300/300" alt="Satellite Image"/>
+        `;
+        this.setMedia(placeholder_media);
     }
 
-    show(mediaContent, title = 'Satellite Data') {
-        const popup = this.querySelector('#satellite-data-panel');
+    // show(mediaContent, title = 'Satellite Data') {
+    show(title = 'Satellite Data') {
+        const popup = this.querySelector('#satellite-data-panel-container');
         const titleElem = this.querySelector('#media-title');
-        const content = this.querySelector('#media-content');
+        // const content = this.querySelector('#media-content');
 
         titleElem.textContent = title;
-        content.innerHTML = mediaContent;
+        // content.innerHTML = mediaContent;
         popup.classList.remove('hidden');
     }
 
     hide() {
-        const popup = this.querySelector('#satellite-data-panel');
+        const popup = this.querySelector('satellite-data-panel');
         popup.classList.add('hidden');
+    }
+
+    setMedia(mediaContent) {
+        const content = this.querySelector('#media-content');
+        content.innerHTML = mediaContent;
     }
 }
 
