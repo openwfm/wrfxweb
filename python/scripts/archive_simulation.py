3692589e0264b49d9e9139248ad80ca4864bc460
Chase Porter <chaseporter15@gmail.com>
2025-08-11 21:21:47 -0700

    Adds a script to archive old timestamps from simulation

---
+++ python/scripts/archive_simulation.py
@@ -0,0 +1,146 @@
+from scripts.scriptKeys import (
+    SIMULATIONS_FOLDER,
+)
+
+from datetime import datetime
+import html
+import os
+import json
+import sys
+
+
+def archive_simulation(job_id, number_of_days):
+    try:
+        manifest_json = process_job_manifest(job_id, number_of_days, False)
+        manifest_path = job_manifest_path(job_id)
+        archive_manifest_path = archive_job_manifest_path(job_id)
+        if os.path.exists(manifest_path):
+            os.rename(manifest_path, archive_manifest_path)
+        with open(manifest_path, "w") as file:
+            json.dump(manifest_json, file, indent=4)
+    except Exception as e:
+        print(f"Error in recreate_manifest: {e}")
+        return
+
+
+def archive_simulation_dry_run(job_id, number_of_days):
+    try:
+        manifest_json = process_job_manifest(job_id, number_of_days, True)
+        dry_run_manifest_path = dry_run_job_manifest_path(job_id)
+        with open(dry_run_manifest_path, "w") as file:
+            json.dump(manifest_json, file, indent=4)
+    except Exception as e:
+        print(f"Error in recreate_manifest: {e}")
+        return
+
+
+def process_job_manifest(job_id, number_of_days, dry_run):
+    manifest_json = load_manifest(job_id)
+    new_manifest = {}
+    count = 0
+    for domain in manifest_json:
+        domain_json = manifest_json[domain]
+        for timestamp in domain_json:
+            timestamp_json = domain_json[timestamp]
+            for layer_type in timestamp_json:
+                layer_json = timestamp_json[layer_type]
+                if timestamp_age_in_days(timestamp) > number_of_days:
+                    if dry_run:
+                        count += 1
+                    else:
+                        delete_timestamp_urls(job_id, layer_json)
+                else:
+                    new_domain_json = inner_manifest(new_manifest, domain)
+                    new_timestamp_json = inner_manifest(new_domain_json, timestamp)
+                    new_layer_json = inner_manifest(new_timestamp_json, layer_type)
+
+                    new_layer_json["raster"] = sanitize_text(layer_json["raster"])
+                    new_layer_json["coords"] = layer_json["coords"]
+                    if "colorbar" in layer_json:
+                        new_layer_json["levels"] = layer_json["levels"]
+                        new_layer_json["colorbar"] = layer_json["colorbar"]
+                    if "kml" in layer_json:
+                        new_layer_json["kml"] = sanitize_text(layer_json["kml"])
+    if dry_run:
+        print(f"Will archive {count} timestamp urls")
+    return new_manifest
+
+
+def delete_timestamp_urls(job_id, layer_json):
+    delete_url(job_id, layer_json["raster"])
+    if "colorbar" in layer_json:
+        delete_url(job_id, layer_json["colorbar"])
+    if "kml" in layer_json:
+        delete_url(job_id, layer_json["kml"])
+
+
+def delete_url(job_id, url_to_delete):
+    simulation_path = job_simulation_path(job_id)
+    url_full_path = os.path.join(simulation_path, url_to_delete)
+    if os.path.exists(url_full_path):
+        os.remove(url_full_path)
+
+
+def job_simulation_path(job_id):
+    simulation_path = os.path.join(SIMULATIONS_FOLDER, job_id)
+    return simulation_path
+
+
+def job_manifest_path(job_id):
+    manifest_filename = f"{job_id}.json"
+    manifest_path = os.path.join(SIMULATIONS_FOLDER, job_id, manifest_filename)
+    return manifest_path
+
+
+def dry_run_job_manifest_path(job_id):
+    manifest_filename = f"{job_id}_dry_run.json"
+    manifest_path = os.path.join(SIMULATIONS_FOLDER, job_id, manifest_filename)
+    return manifest_path
+
+
+def archive_job_manifest_path(job_id):
+    date = datetime.now().strftime("%Y%m%d")
+    manifest_filename = f"{job_id}_{date}.json"
+    manifest_path = os.path.join(SIMULATIONS_FOLDER, job_id, manifest_filename)
+    return manifest_path
+
+
+def timestamp_age_in_days(timestamp):
+    date_format = "%Y-%m-%d_%H:%M:%S"
+    datetime_timestamp = datetime.strptime(timestamp, date_format)
+    return (datetime.now() - datetime_timestamp).days
+
+
+def load_manifest(job_id):
+    manifest_path = job_manifest_path(job_id)
+    try:
+        manifest_json = json.load(open(manifest_path))
+        return manifest_json
+    except Exception as e:
+        print(f"Problem loading manifest at {manifest_path}: {e}")
+        return {}
+
+
+def inner_manifest(manifest, key):
+    key = sanitize_text(key)
+    if key in manifest:
+        return manifest[key]
+    inner_manifest = {}
+    manifest[key] = inner_manifest
+    return inner_manifest
+
+
+def sanitize_text(text_input):
+    return html.escape(text_input)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(
+            "Please provide a job_id, number_of_days to keep, and dry_run boolean of a simulation to archive"
+        )
+    else:
+        if sys.argv[3] == "0":
+            archive_simulation(sys.argv[1], int(sys.argv[2]))
+        else:
+            archive_simulation_dry_run(sys.argv[1], int(sys.argv[2]))
