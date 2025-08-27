a0791e7bc7191cf6e7238e503f4a73bf369c407b
Chase Porter <chaseporter15@gmail.com>
2025-08-27 15:31:39 -0700

    Adds post for failed actions back to the queue

--- python/worker/services/queue_services.py
+++ python/worker/services/queue_services.py
@@ -10,7 +10,17 @@ LOGGING_AREA = "QueueServices"
 
 class QueueServices:
     def post_failed_action(self, action_params):
-        return -1
+        post_url = f"{UPLOAD_QUEUE_SERVICE_URL}/fail"
+        try:
+            headers = {
+                "Content-type": "application/json",
+                "API-Key": UPLOAD_QUEUE_SERVICE_API_KEY,
+            }
+            response = requests.post(post_url, headers=headers, json=action_params)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            self.log_queue_service_error(f"{e}")
+            return -1
 
     def get_dequeue(self):
         get_url = f"{UPLOAD_QUEUE_SERVICE_URL}/dequeue"
@@ -22,17 +32,9 @@ class QueueServices:
             response = requests.get(get_url, headers=headers)
             response.raise_for_status()
             if response.status_code == 200:
-                next_catalog_entry_upload_id = response.json()[
-                    "catalog_entry_upload_id"
-                ]
-                if not next_catalog_entry_upload_id.isdigit():
-                    error = f"Invalid catalog_entry_upload_id: {next_catalog_entry_upload_id}"
-                    self.log_queue_service_error(error)
-                    return -1
-                self.log_queue_service(
-                    f"catalog_entry_upload_id: {next_catalog_entry_upload_id}"
-                )
-                return int(next_catalog_entry_upload_id)
+                next_action_params = response.json()
+                self.log_queue_service("Next Action Retreived")
+                return next_action_params
             elif response.status_code == 204:
                 self.log_queue_service("UploadQueueService is empty")
                 return None
